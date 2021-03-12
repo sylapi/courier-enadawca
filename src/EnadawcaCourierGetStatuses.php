e3fcfb0e467acaddfb5de441aa67ae8c05391eb1
Daniel Serafinski <d.serafinski@sylapi.com>
2021-03-12 14:35:00 +0100

    Fix

--- src/EnadawcaCourierGetStatuses.php
+++ src/EnadawcaCourierGetStatuses.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sylapi\Courier\Enadawca;
 
 use SoapFault;
+use Exception;
 use Sylapi\Courier\Contracts\CourierGetStatuses;
 use Sylapi\Courier\Contracts\Status as StatusContract;
 use Sylapi\Courier\Entities\Status;
@@ -38,15 +39,16 @@ class EnadawcaCourierGetStatuses implements CourierGetStatuses
             $statusName = end($statuses);
 
             if (!$statusName || !isset($statusName->kod)) {
-                $statusName = StatusType::APP_RESPONSE_ERROR;
-                $status = new Status((string) $statusName);
-                $e = new TransportException('Error code: '.$response->return->status);
-                ResponseHelper::pushErrorsToResponse($status, [$e]);
-            } else {
-                $status = new Status((string) new EnadawcaStatusTransformer($statusName->kod));
+                throw new TransportException('Error code: '.$response->return->status);
             }
+            
+            $status = new Status((string) new EnadawcaStatusTransformer($statusName->kod));
+
         } catch (SoapFault $fault) {
             $excaption = new TransportException($fault->faultstring);
+            $status = new Status(StatusType::APP_RESPONSE_ERROR);
+            ResponseHelper::pushErrorsToResponse($status, [$excaption]);
+        } catch (Exception $excaption) {
             $status = new Status(StatusType::APP_RESPONSE_ERROR);
             ResponseHelper::pushErrorsToResponse($status, [$excaption]);
         }

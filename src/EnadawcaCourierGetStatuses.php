90a23fc0e2a558da7493bb72d579631ea6961610
Daniel Serafinski <d.serafinski@sylapi.com>
2021-03-12 14:23:50 +0100

    Fix

--- src/EnadawcaCourierGetStatuses.php
+++ src/EnadawcaCourierGetStatuses.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sylapi\Courier\Enadawca;
 
+use SoapFault;
 use Sylapi\Courier\Contracts\CourierGetStatuses;
 use Sylapi\Courier\Contracts\Status as StatusContract;
 use Sylapi\Courier\Entities\Status;
@@ -23,25 +24,30 @@ class EnadawcaCourierGetStatuses implements CourierGetStatuses
     public function getStatus(string $shipmentId): StatusContract
     {
         $client = $this->session->clientTracking();
+        try {
+            $response = $client->__soapCall(
+                'sprawdzPrzesylkePl',
+                [
+                    'sprawdzPrzesylke' => ['numer' => $shipmentId],
+                ]
+            );
 
-        $response = $client->__soapCall(
-            'sprawdzPrzesylkePl',
-            [
-                'sprawdzPrzesylke' => ['numer' => $shipmentId],
-            ]
-        );
+            $statuses = $response->return->danePrzesylki->zdarzenia->zdarzenie ?? [];
 
-        $statuses = $response->return->danePrzesylki->zdarzenia->zdarzenie ?? [];
+            $statusName = end($statuses);
 
-        $statusName = end($statuses);
-
-        if (!$statusName || !isset($statusName->kod)) {
-            $statusName = StatusType::APP_RESPONSE_ERROR;
-            $status = new Status((string) $statusName);
-            $e = new TransportException('Error code: '.$response->return->status);
-            ResponseHelper::pushErrorsToResponse($status, [$e]);
-        } else {
-            $status = new Status((string) new EnadawcaStatusTransformer($statusName->kod));
+            if (!$statusName || !isset($statusName->kod)) {
+                $statusName = StatusType::APP_RESPONSE_ERROR;
+                $status = new Status((string) $statusName);
+                $e = new TransportException('Error code: '.$response->return->status);
+                ResponseHelper::pushErrorsToResponse($status, [$e]);
+            } else {
+                $status = new Status((string) new EnadawcaStatusTransformer($statusName->kod));
+            }
+        } catch (SoapFault $fault) {
+            $excaption = new TransportException($fault->faultstring);
+            $status = new Status(StatusType::APP_RESPONSE_ERROR);
+            ResponseHelper::pushErrorsToResponse($status, [$excaption]);
         }
 
         return $status;
